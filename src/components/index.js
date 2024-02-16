4079183553b73b1a09e322bc8c5c67f12bf0b3c0
Adarsh Kumar Pandey <fakp24@gmail.com>
2024-02-16 17:02:53 +0530

    auth service

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-import Header from "./Header/header";
-import Footer from "./Footer/footer";
+import Header from "./Header";
+import Footer from "./Footer";
 import Container from './container/Container';
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
